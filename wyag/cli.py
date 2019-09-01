d7d40b9f20acf05f37f15605d55b52d4f73d2217
Carlton Duffett <carlton.duffett@gmail.com>
2019-08-31 18:16:15 -0700

    Add cat-file command

--- wyag/cli.py
+++ wyag/cli.py
@@ -4,7 +4,9 @@ import sys
 
 import click
 
-from . import services
+from . import models, services
+
+VERSION = "0.1.0"
 
 
 @click.group()
@@ -16,18 +18,26 @@ def wyag():
 @click.command()
 def version():
     """Print this project's version and exit."""
-    with open(".version") as f:
-        print(f.readline())
+    print(VERSION)
 
 
 @click.command()
 @click.argument("path", default=".")
 def init(path):
     """Initialize a new, empty repository."""
+    services.repo.init_repo(path)
 
-    services.init_repo(path)
+
+@click.command("cat-file")
+@click.argument("object_", metavar="OBJECT")
+def cat_file(object_):
+    """Print the specified OBJECT."""
+
+    repo = models.repo.Repository.find()
+    services.objects.cat_object(repo, object_)
 
 
 # Add all of the subcommands to the main group
 wyag.add_command(version)
 wyag.add_command(init)
+wyag.add_command(cat_file)
