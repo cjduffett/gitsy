96d206c586e62e02fc2fe2c187b2c581fae84122
Carlton Duffett <carlton.duffett@gmail.com>
2019-08-31 22:24:53 -0700

    Implement parse_message()

--- wyag/services/objects.py
+++ wyag/services/objects.py
@@ -3,7 +3,7 @@
 import hashlib
 import zlib
 from pathlib import Path
-from typing import Type
+from typing import Optional, Type
 
 from .. import models
 
@@ -21,17 +21,17 @@ def read_object(repo: models.Repository, sha: str) -> models.objects.Object:
         raw_data = zlib.decompress(f.read())
 
     # Read object type
-    obj_type_separator = raw_data.find(b" ")
-    obj_type = raw_data[0:obj_type_separator].decode("utf-8")
+    space_index = raw_data.find(b" ")
+    obj_type = raw_data[0:space_index].decode("utf-8")
 
     # Read and validate object size
-    obj_size_separator = raw_data.find(b"\x00")
-    obj_size = raw_data[obj_type_separator:obj_size_separator].decode("utf-8")
+    null_index = raw_data.find(b"\x00")
+    obj_size = raw_data[space_index:null_index].decode("utf-8")
 
-    if int(obj_size) != len(raw_data) - obj_size_separator - 1:
+    if int(obj_size) != len(raw_data) - null_index - 1:
         raise Exception(f"Malformed object {sha}: bad length")
 
-    obj_data = raw_data[obj_size_separator + 1 :]
+    obj_data = raw_data[null_index + 1 :]
     obj_class = _get_object_class(obj_type)
     return obj_class(repo, obj_data)
 
@@ -84,6 +84,84 @@ def hash_object(
     obj = obj_class(repo, obj_data)
 
     return write_object(obj, write=write)
+
+
+def parse_message(raw_data: bytes, start: int = 0, value_lookup: Optional[dict] = None) -> dict:
+    """Parse as Git Commit or Tag message body as a series of key-value pairs.
+
+    Commits (and Tags) are formatted like:
+
+    ```
+    tree 29ff16c9c14e2652b22f8b78bb08a5a07930c147
+    parent 206941306e8a8af65b66eaaaea388a7ae24d49a0
+    author Carlton Duffett <carlton.duffett@gmail.com> 1527025023 -0700
+    committer Carlton Duffett <carlton.duffett@gmail.com> 1527025044 -0700
+    gpgsig -----BEGIN PGP SIGNATURE-----
+     iQIzBAABCAAdFiEExwXquOM8bWb4Q2zVGxM2FxoLkGQFAlsEjZQACgkQGxM2FxoL
+     kGQdcBAAqPP+ln4nGDd2gETXjvOpOxLzIMEw4A9gU6CzWzm+oB8mEIKyaH0UFIPh
+     rNUZ1j7/ZGFNeBDtT55LPdPIQw4KKlcf6kC8MPWP3qSu3xHqx12C5zyai2duFZUU
+     wqOt9iCFCscFQYqKs3xsHI+ncQb+PGjVZA8+jPw7nrPIkeSXQV2aZb1E68wa2YIL
+     3eYgTUKz34cB6tAq9YwHnZpyPx8UJCZGkshpJmgtZ3mCbtQaO17LoihnqPn4UOMr
+     V75R/7FjSuPLS8NaZF4wfi52btXMSxO/u7GuoJkzJscP3p4qtwe6Rl9dc1XC8P7k
+     NIbGZ5Yg5cEPcfmhgXFOhQZkD0yxcJqBUcoFpnp2vu5XJl2E5I/quIyVxUXi6O6c
+     /obspcvace4wy8uO0bdVhc4nJ+Rla4InVSJaUaBeiHTW8kReSFYyMmDCzLjGIu1q
+     doU61OM3Zv1ptsLu3gUE6GU27iWYj2RWN3e3HE4Sbd89IFwLXNdSuM0ifDLZk7AQ
+     WBhRhipCCgZhkj9g2NEk7jRVslti1NdN5zoQLaJNqSwO1MtxTmJ15Ksk3QP6kfLB
+     Q52UWybBzpaP9HEd4XnR+HuQ4k2K0ns2KgNImsNvIyFwbpMUyUWLMPimaV1DWUXo
+     5SBjDB/V/W2JBFR+XKHFJeFwYhj7DD/ocsGr4ZMx/lgc8rjIBkI=
+     =lgTX
+     -----END PGP SIGNATURE-----
+
+    Initial commit
+    ```
+
+    A single value may be continued on several lines, where each subsequent line starts with
+    an additional space (see gpgsig as an example of that).
+    """
+
+    if not value_lookup:
+        value_lookup = dict()
+
+    # Find the next space and newline, deliniating another key-value pair.
+    space_index = raw_data.find(b" ", start)
+    newline_index = raw_data.find(b"\n", start)
+
+    # If a space appears before a newline, we have a keyword.
+
+    # If a newline appears first, or there's no space at all (-1), we assume a blank line.
+    # A blank line means the remainder of the data is the message.
+    if (space_index < 0) or (newline_index < space_index):
+        value_lookup[b""] = raw_data[start + 1 :]
+        return value_lookup
+
+    # Read the next key in the body
+    key = raw_data[start:space_index]
+
+    # Read the value after that key
+    end = start
+
+    while True:
+        # Continuation lines begin with a space. Continue collecting the value until
+        # we encounter a newline immediately followed by a space.
+        end = raw_data.find(b"\n", end + 1)
+        if raw_data[end + 1] == b" ":
+            break
+
+    # Extract the value and drop leading spaces on continuation lines.
+    value = raw_data[space_index + 1 : end].replace(b"\n ", b"\n")
+
+    # Repeat instances of a key become a list
+    try:
+        existing_value = value_lookup[key]
+    except KeyError:
+        value_lookup[key] = value
+    else:
+        if isinstance(existing_value, list):
+            value_lookup[key].append(value)
+        else:
+            value_lookup[key] = [existing_value, value]
+
+    return parse_message(raw_data, start=end + 1, value_lookup=value_lookup)
 
 
 def resolve_sha(repo: models.Repository, name: str, obj_type: str, follow: bool = True) -> str:
