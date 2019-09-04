cd459a9671fae398a4fcb17559d93ac0aa7ef993
Carlton Duffett <carlton.duffett@gmail.com>
2019-09-03 20:14:44 -0700

    More stub object service tests

---
+++ tests/services/test_objects.py
@@ -0,0 +1,89 @@
+"""Object service tests."""
+
+
+def test_read_object__ok():
+    pass
+
+
+def test_read_object__not_found():
+    pass
+
+
+def test_read_object__type_mismatch():
+    pass
+
+
+def test_read_object__malformed():
+    pass
+
+
+def test_cat_object__full_sha():
+    pass
+
+
+def test_cat_object__resolve_name():
+    pass
+
+
+def test_write_object__dry_run():
+    pass
+
+
+def test_write_object__commit():
+    pass
+
+
+def test_hash_object__not_found():
+    pass
+
+
+def test_hash_object__full_sha():
+    pass
+
+
+def test_resolve_oblect__full_sha():
+    pass
+
+
+def test_resolve_oblect__short_sha():
+    pass
+
+
+def test_resolve_oblect__head():
+    pass
+
+
+def test_resolve_object__ref():
+    pass
+
+
+def test_find_object__no_results():
+    pass
+
+
+def test_find_object__multiple_results():
+    pass
+
+
+def test_find_object__no_obj_type():
+    pass
+
+
+def test_find_object__dont_follow():
+    pass
+
+
+def test_find_object__follow_tag():
+    pass
+
+
+def test_find_object__follow_commit():
+    pass
+
+
+def test_log_history():
+    pass
+
+
+def test_log_commit():
+    pass
