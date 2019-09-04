327442ba0f23bcb6436103bd748db36db8763af8
Carlton Duffett <carlton.duffett@gmail.com>
2019-09-03 20:13:20 -0700

    Stub create_tag()

--- wyag/services/tags.py
+++ wyag/services/tags.py
@@ -2,10 +2,37 @@
 
 from typing import Optional
 
-from .objects import write_object
+from ..models.objects import Tag
+from ..models.repo import Repository
+from .objects import resolve_object
+from .refs import create_ref
 
 
-def create_tag(name: str, obj_sha: Optional[str] = None, tag_type: str = "ref"):
-    """Creat a new tag with the given name, optionally referencing the specified object."""
+def create_tag(
+    repo: Repository,
+    name: str,
+    obj_sha: Optional[str] = None,
+    annotate: bool = False,
+    message: Optional[str] = None,
+):
+    """Creat a new tag with the given name, optionally referencing the specified object.
 
-    # TODO: implementation
+    There are 2 flavors of tags:
+    1. "Lightweight" tags  - refs to a commits, trees, or blobs.
+    2. Tag objects - store an author, date, and message, like commits.
+
+    The tag `message` is ignored if `annotate = False`.
+    """
+
+    if annotate and not message:
+        raise Exception("A 'message' is required to annotate a tag")
+
+    if annotate:
+        # Create a tag object
+        tag = Tag(repo)
+
+    else:
+        # Create a "lightweight" tag ref
+        ref_name = f"tags/{name}"
+        full_sha = resolve_object(repo, obj_sha)
+        create_ref(repo, ref_name, sha=full_sha)
